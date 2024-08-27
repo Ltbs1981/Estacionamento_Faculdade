c12e6ddab3f7e7fc711b8afba936cccfc9205b11
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-08-27 12:30:50 -0300

    Documentation

--- src/main/java/com/thykkannotech/ConexaoBD.java
+++ src/main/java/com/thykkannotech/ConexaoBD.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConexaoBD {
     private static final String URL = "jdbc:mysql://localhost:3306/estacionamento";
     private static final String USUARIO = "root";
-    private static final String SENHA = "SUA_SENHA@";
+    private static final String SENHA = "SUA_SENHA";
 
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, SENHA);
